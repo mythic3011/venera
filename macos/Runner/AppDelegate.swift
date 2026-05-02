8210a993265ba435a4434163f7d6c3a1b0a268be
Mythic3011 <42365807+mythic3011@users.noreply.github.com>
2026-05-03 03:37:20 +0800

    fix(macos): declare secure restorable state support

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -96,4 +96,8 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
 }
